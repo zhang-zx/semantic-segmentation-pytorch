ff122eea9a07a5a0504f3ed35d0b503d72d76c3a
Zhixing Zhang <zhangzx15@126.com>
2019-03-06 11:16:15 +0800

    init

--- coco_train.py
+++ coco_train.py
@@ -179,66 +179,8 @@ def main(args):
         segmentation_module = SegmentationModule(
             net_encoder, net_decoder, crit)
 
-    # Dataset and Loader
-    dataset_type = 'CocoDataset'
-    data_root = 'data/coco/'
-    img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[1, 1, 1], to_rgb=True)
-    data = dict(
-        imgs_per_gpu=8,
-        workers_per_gpu=3,
-        train=dict(
-            type='RepeatDataset',
-            times=5,
-            dataset=dict(
-                type=dataset_type,
-                ann_file=data_root + 'annotations/instances_train2017.json',
-                img_prefix=data_root + 'train2017/',
-                img_scale=(300, 300),
-                img_norm_cfg=img_norm_cfg,
-                size_divisor=None,
-                flip_ratio=0.5,
-                with_mask=False,
-                with_crowd=False,
-                with_label=True,
-                test_mode=False,
-                extra_aug=dict(
-                    photo_metric_distortion=dict(
-                        brightness_delta=32,
-                        contrast_range=(0.5, 1.5),
-                        saturation_range=(0.5, 1.5),
-                        hue_delta=18),
-                    expand=dict(
-                        mean=img_norm_cfg['mean'],
-                        to_rgb=img_norm_cfg['to_rgb'],
-                        ratio_range=(1, 4)),
-                    random_crop=dict(
-                        min_ious=(0.1, 0.3, 0.5, 0.7, 0.9), min_crop_size=0.3)),
-                resize_keep_ratio=False)),
-        val=dict(
-            type=dataset_type,
-            ann_file=data_root + 'annotations/instances_val2017.json',
-            img_prefix=data_root + 'val2017/',
-            img_scale=(300, 300),
-            img_norm_cfg=img_norm_cfg,
-            size_divisor=None,
-            flip_ratio=0,
-            with_mask=False,
-            with_label=False,
-            test_mode=True,
-            resize_keep_ratio=False),
-        test=dict(
-            type=dataset_type,
-            ann_file=data_root + 'annotations/instances_val2017.json',
-            img_prefix=data_root + 'val2017/',
-            img_scale=(300, 300),
-            img_norm_cfg=img_norm_cfg,
-            size_divisor=None,
-            flip_ratio=0,
-            with_mask=False,
-            with_label=False,
-            test_mode=True,
-            resize_keep_ratio=False))
-    train_dataset = get_dataset(data["train"])
+    cfg = Config.fromfile("config.py")
+    train_dataset = get_dataset(cfg.data.train)
     # data_loaders = [
     #     build_dataloader(
     #         train_dataset,
